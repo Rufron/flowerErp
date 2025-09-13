d3c5abbd1e9f20ff0bd2a13e3d5d371ae3664eb5
rufron <kipchumbabrian572@gmail.com>
2025-09-13 12:28:03 +0300

    without vite

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,3 @@
-// import { defineConfig } from "vite";
-// import laravel from "laravel-vite-plugin";
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ["resources/css/app.css", "resources/js/app.js"],
-//             refresh: true,
-//         }),
-//     ],
-
-//     build: {
-//         manifest: true, // ✅ tells Vite to generate manifest.json
-//         outDir: "public/build", // ✅ Laravel expects it here
-//         assetsDir: "assets", // ✅ will output to public/build/assets
-//     },
-
-//     base: process.env.APP_ENV === "production" ? "/build/" : "/",
-// });
-
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
@@ -29,9 +9,4 @@ export default defineConfig({
         }),
     ],
 
-    build: {
-        manifest: true,
-        outDir: 'public/build',  // ✅ ensure manifest.json is here
-        emptyOutDir: true,       // ✅ clears old files
-    },
 });
